29da6bdec4b2252a64fb4594b614f56ddd3164e5
Jona909090 <jonic.ctba@gmail.com>
2026-07-17 19:23:19 +0200

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,1 +1,52 @@
-
+export const materials = [
+  {
+    id: 1,
+    name: "PLYWOOD",
+    standard: "DIN7500M TX M6X40 Zn",
+    mv: 180,
+    mvs: 150,
+    rpp: 350,
+    stock: 2500,
+    unit: "kom"
+  },
+  {
+    id: 2,
+    name: "CETRIS",
+    standard: "DIN7505A 5X40 Zn",
+    mv: 180,
+    mvs: 150,
+    rpp: 350,
+    stock: 1800,
+    unit: "kom"
+  },
+  {
+    id: 3,
+    name: "WALL PANELS",
+    standard: "JT3-D6H-5,5/6,3x127",
+    mv: 250,
+    mvs: 200,
+    rpp: 2000,
+    stock: 5100,
+    unit: "kom"
+  },
+  {
+    id: 4,
+    name: "CEILING PANELS",
+    standard: "JT3-D6H-5,5/6,3x127",
+    mv: 250,
+    mvs: 200,
+    rpp: 400,
+    stock: 3200,
+    unit: "kom"
+  },
+  {
+    id: 5,
+    name: "EXTERNAL PANELS",
+    standard: "JT3-12-5,5x40",
+    mv: 240,
+    mvs: 200,
+    rpp: 600,
+    stock: 2600,
+    unit: "kom"
+  }
+];
