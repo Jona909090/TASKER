e0c524224c2b89f11054b14179c1efc9cb499327
Jona909090 <jonic.ctba@gmail.com>
2026-07-28 20:39:50 +0200

    Set project 2 access code to 7071

--- drive-endpoint-v52.js
+++ drive-endpoint-v52.js
@@ -1,4 +1,4 @@
-// TASKER v52: preusmerava Drive zahteve na trenutno aktivno Apps Script raspoređivanje.
+// TASKER v53: aktivna Drive veza i posebna lozinka za projekat 02.
 (() => {
   const previousEndpoint = 'https://script.google.com/macros/s/AKfycbzF2SfEXGL137WzWsbhV6ElBEdqcVBbr5AFooCXWdtmuef-3pqaw2HCGnyUlFjoooTRHQ/exec'
   const activeEndpoint = 'https://script.google.com/macros/s/AKfycbxg35McxrxumR3uX15gbGlrhkiRRhDTUzTWVbqeKwMOskm2DI47U_-OqwY64FfQwrFUSw/exec'
@@ -8,4 +8,15 @@
     else if (input instanceof Request && input.url.includes(previousEndpoint)) input = new Request(input.url.replace(previousEndpoint, activeEndpoint), input)
     return nativeFetch(input, init)
   }
+
+  let receiptProjectUnlock = false
+  document.addEventListener('click', (event) => {
+    if (event.target.closest('#open-receipts-project')) receiptProjectUnlock = true
+    else if (event.target.closest('.project-card')) receiptProjectUnlock = false
+  }, true)
+  document.addEventListener('submit', (event) => {
+    if (!receiptProjectUnlock || event.target.id !== 'project-password-form') return
+    const input = event.target.querySelector('#project-password-input')
+    if (input && input.value === '7071') input.value = '7070'
+  }, true)
 })()
