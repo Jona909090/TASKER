687d1c36d69f89be1ed1746b402830db3dc6b69a
Jona <jonic.ctba@gmail.com>
2026-09-13 18:19:32 +0200

    Release Tasker v87 end-of-employment table colors

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'tasker-v86'
+const CACHE = 'tasker-v87'
 const CORE = [
   './employee-hours-model-v86.js?build=86',
-  './employee-hours-v86.js?build=86',
+  './employee-hours-v87.js?build=87',
   './',
   './index.html',
   './manifest.webmanifest',
