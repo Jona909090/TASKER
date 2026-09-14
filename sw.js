5f1b94a4105428925312f3d7bfce3e9b5628da25
Jona <jonic.ctba@gmail.com>
2026-09-14 19:57:22 +0200

    Publish document import into attendance archive v101

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'tasker-v100'
+const CACHE = 'tasker-v101'
 const CORE = [
-  './employee-hours-archive-v100.js?build=100',
+  './employee-hours-archive-v101.js?build=101',
   './employee-hours-books-v97.js?build=97',
   './employee-hours-model-v94.js?build=94',
   './employee-hours-export-v100.js?build=100',
